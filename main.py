8e8c8089cc611365f2e421bf1833d37ba9b5cf6f
KIBI <56989035+srKIBI@users.noreply.github.com>
2022-06-30 23:11:19 -0300

    Adicionado comando "sair" para sair do shell

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import base64, codecs
-magic = 'aW1wb3J0IGxlcGFleGluCgp3aGlsZSBUcnVlOgoJdGV4dCA9IGlucHV0KCdDb3hpbmhhIC8+ICAnKQoJaWYgdGV4dC5zdHJpc'
-love = 'PtcVQ09VPVvBvOwo250nJ51MDbWpzImqJk0YPOypaWipvN9VTkypTSyrTyhYaW1ovtaCUA0MTyhCvpfVUEyrUDcPtbWnJLtMK'
-god = 'Jyb3I6CgkJcHJpbnQoZXJyb3IuYXNfc3RyaW5nKCkpCgllbGlmIHJlc3VsdDoKCQlpZiBsZW4ocmVzdWx0LmVsZW1lbnRzKSA'
-destiny = '9CFNkBtbWPDyjpzyhqPulMKOlXUWyp3IfqP5yoTIgMJ50p1fjKFxcPtxWMJkmMGbXPDxWpUWcoaDbpzIjpvulMKA1oUDcXDb='
+magic = 'aW1wb3J0IGxlcGFleGluCgp3aGlsZSBUcnVlOgoJdGV4dCA9IGlucHV0KCdDb3hpbmhhIC8+ICAnKQoJaWYgdGV4dC5zdHJpcCgpID09ICIi'
+love = 'BvOwo250nJ51MDbWnJLtqTI4qP5mqUWcpPtcVQ09VPWmLJylVwbtLaWyLJfXPKWyp3IfqPjtMKWlo3VtCFOfMKOuMKucov5lqJ4bWmkmqTEc'
+god = 'bj4nLCB0ZXh0KQoKCWlmIGVycm9yOgoJCXByaW50KGVycm9yLmFzX3N0cmluZygpKQoJZWxpZiByZXN1bHQ6CgkJaWYgbGVuKHJlc3VsdC5l'
+destiny = 'oTIgMJ50plxtCG0tZGbXPDxWpUWcoaDbpzIjpvulMKA1oUDhMJkyoJIhqUAoZS0cXDbWPJIfp2H6PtxWPKOlnJ50XUWypUVbpzImqJk0XFxX'
 joy = '\x72\x6f\x74\x31\x33'
 trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
 eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
